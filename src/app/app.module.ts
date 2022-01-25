1e79351c06d29aa7c31d1d82f9a3c4287653a08c
Matthew <matthew3chapman@gmail.com>
2022-01-25 16:41:44 -0500

    Created footer and minor tweaks to the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 import { Header } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Header
+    Header,
+    FooterComponent
   ],
   imports: [
     BrowserModule
