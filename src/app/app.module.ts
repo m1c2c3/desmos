7602c71bed8f9371f9c4bcaef01745f27380d99d
Matthew <matthew3chapman@gmail.com>
2022-02-01 15:38:14 -0500

    Added the card components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Card1Component } from './card1/card1.component';
+import { Card2Component } from './card2/card2.component';
 import { FooterComponent } from './footer/footer.component';
 import { Footer2Component } from './footer2/footer2.component';
 import { Header } from './header/header.component';
@@ -13,7 +14,8 @@ import { Header } from './header/header.component';
     Header,
     FooterComponent,
     Footer2Component,
-    Card1Component
+    Card1Component,
+    Card2Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/card2/card2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+
+    selector: 'desmos-card2',
+    templateUrl: 'card2.component.html'
+})
+export class Card2Component{
+
+}
